62f8bd0d3d763e480cd8d04531f9dc775d7a3c6e
jiyoung <higomn05@naver.com>
2022-06-10 11:12:48 +0900

    excel 내 오류데이터 style 적용

--- src/components/ExportExcel_style.js
+++ src/components/ExportExcel_style.js
@@ -2,7 +2,35 @@ import React from 'react'
 import * as ExcelJS from "exceljs";
 import { saveAs } from "file-saver";
 
+const regDataStyle = (item, idx) => {
 
+  const regExpCorpid = /^[0-9]*$/;
+  const regExpUserid = /^[a-zA-Z0-9]*$/;
+  const regExpUsername = /^[가-힣a-zA-Z]+$/;
+  const regExpPhone = /^[0-9]*$/;
+
+  if(item === '') return false
+  else {
+    if(idx === 0) {
+      // 회사명
+      if(!regExpCorpid.test(item)) return false
+    } 
+    if(idx === 1) {
+      // 아이디
+      if(!regExpUserid.test(item)) return false
+    } 
+    if(idx === 2) {
+      // 이름
+      if(!regExpUsername.test(item)) return false
+    }
+    if(idx === 3) {
+      // 전화번호
+      if(!regExpPhone.test(item)) return false
+    }
+  }
+
+  return true;
+}
 
 async function ExportExcel_style(data) {
     const workbook = new ExcelJS.Workbook();
@@ -18,7 +46,7 @@ async function ExportExcel_style(data) {
     for(let i=1; i<data['result'].length; i++){
       let Rowdata = data['result'][i].map((element, idx) => {
         return (
-          element
+          element === undefined ? '' : element
         )
       });
       worksheet.addRow(Rowdata)
@@ -28,7 +56,7 @@ async function ExportExcel_style(data) {
     worksheet.eachRow({ includeEmpty: true }, (row, rowNumber) => {
       row.eachCell({ includeEmpty: true }, (cell, colNumber) => {
         
-        if (rowNumber == 1) {
+        if (rowNumber === 1) {
           // First set the background of header row
           cell.fill = {
             type: 'pattern',
@@ -54,6 +82,23 @@ async function ExportExcel_style(data) {
           bottom: { style: 'thin' },
           right: { style: 'thin' }
         };
+
+        // error data
+        if(rowNumber !== 1 && regDataStyle(cell.value, colNumber-1) === false){
+          cell.style = {
+            border: {
+              top: { style: 'thin' },
+              left: { style: 'thin' },
+              bottom: { style: 'thin' },
+              right: { style: 'thin' }
+            },
+            fill : {
+              type: 'pattern',
+              pattern: 'solid',
+              fgColor: { argb: 'f8bbd0'}
+            }
+          }
+        }
       })
     })
     
